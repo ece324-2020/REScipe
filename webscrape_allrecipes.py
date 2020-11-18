fd7072bc1852089d04bf9e93f53a74a6a76a4ae2
Shawn <xiaoen.zhang@mail.utoronto.ca>
2020-11-17 23:12:20 -0500

    Added more to total data, removed redundant csvs

--- webscrape_allrecipes.py
+++ webscrape_allrecipes.py
@@ -1,43 +1,44 @@
 from recipe_scrapers import scrape_me
+from threading import Thread
+from multiprocessing import Process
 import numpy as np
-import pymongo
 import pandas as pd
 import csv  
+import time
     
 # give the url as a string, it can be url from any site listed below
 #scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
-# Q: What if the recipe site I want to extract information from is not listed below?
-# A: You can give it a try with the wild_mode option! If there is Schema/Recipe available it will work just fine.
-#scraper = scrape_me('https://www.feastingathome.com/tomato-risotto/', wild_mode=True)
-recipes = []
-rows = []
-for i in range(90000,90200):
-    scraper = scrape_me('https://www.allrecipes.com/recipe/'+str(i)+'/')      
-    if scraper.image() != "" and scraper.image() != 'https://images.media-allrecipes.com/images/79591.png':
-        recipes.append([scraper.title(), scraper.image()])
-        rows.append([str(i)])
-        print(i)
-    if i % 100 == 0:
-        print(i)
-
-# field names  
-fields = ['Good Indices']  
-# name of csv file  
-recipe_numbers = "recipe_numbers_with_images.csv"
-    
-# writing to csv file  
-with open(recipe_numbers, 'w') as csvfile:  
-    csvwriter = csv.writer(csvfile)  
-    csvwriter.writerow(fields)  
-    csvwriter.writerows(rows) 
-
-df=pd.DataFrame(recipes,columns=['Titles','Images'])
-csv_data = df.to_csv('bare_minimum.csv', index = True) 
-#scraper.title()
 #scraper.total_time()
 #scraper.yields()
 #scraper.ingredients()
 #scraper.instructions()
-#scraper.image()
 #scraper.host()
 #scraper.links()
+def check_recipes(start,end,filename):
+    recipes = []
+
+    for i in range(start,end): 
+        scraper = scrape_me('https://www.allrecipes.com/recipe/'+str(i)+'/')      
+        if scraper.image() != "" and scraper.image() != 'https://images.media-allrecipes.com/images/79591.png':
+            recipes.append([scraper.title(), scraper.image(), scraper.total_time(), scraper.yields(), scraper.ingredients(), scraper.instructions(), str(i)])
+            print(i)
+        if i % 100 == 0:
+            print(i)
+
+            # Write Recipe info to csv file
+            df=pd.DataFrame(recipes,columns=['Title','Image', 'Total Time', 'Yields', 'Ingredients', 'Instructions', 'URL'])
+            csv_data = df.to_csv(filename, index = True, mode='a', header=False)
+            recipes = []
+
+
+if __name__ == '__main__':
+
+    Thread(target=check_recipes,args=(70000,80000,'recipes1.csv')).start()
+    time.sleep(3)
+    Thread(target=check_recipes,args=(80000,90000,'recipes2.csv')).start()
+    time.sleep(3)
+    Thread(target=check_recipes,args=(90000,100000,'recipes3.csv')).start()
+    time.sleep(3)
+    Thread(target=check_recipes,args=(100000,110000,'recipes4.csv')).start()
+    time.sleep(3)
+    
